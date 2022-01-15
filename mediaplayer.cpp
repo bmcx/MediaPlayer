5c802d3a597603fc1d74a083f97b3ade58ee7bcf
Chandima Bandara <bmcbandara@hotmail.com>
2022-01-15 17:28:11 +0530

    add: menu items

--- mediaplayer.cpp
+++ mediaplayer.cpp
@@ -10,15 +10,20 @@ MediaPlayer::MediaPlayer(QWidget *parent)
 
     connect(player->instance, SIGNAL(positionChanged(qint64)), this, SLOT(on_positionChanged(qint64)));
     connect(player->instance, SIGNAL(durationChanged(qint64)), this, SLOT(on_durationChanged(qint64)));
+    connect(player->instance, SIGNAL(currentMediaChanged(QMediaContent)), this, SLOT(on_mediaChanged()));
+
+    connect(player->instance->playlist(), SIGNAL(mediaChanged(int,int)), this, SLOT(on_playlistUpdated()));
 
     //    ui->statusbar->addWidget(widget,1);
 
-    // ui button conections
+    // ui button connections
     connect(ui->btnPrev, SIGNAL(clicked()), player->instance->playlist(), SLOT(previous()));
     connect(ui->btnNext, SIGNAL(clicked()), player->instance->playlist(), SLOT(next()));
 
     // set default path to home dir
     lastPath = QDir::homePath();
+//    playlistModel = new QStringListModel(this);
+
 }
 
 MediaPlayer::~MediaPlayer()
@@ -38,18 +43,25 @@ void MediaPlayer::on_sliderVolume_sliderMoved(int position)
 
 void MediaPlayer::on_positionChanged(qint64 position)
 {
-    //    qDebug() << position;
+//    qDebug() << position;
     ui->sliderProgress->setValue(position);
     ui->lblCurrentTime->setText(Utils::formatDuration(position));
 }
 
 void MediaPlayer::on_durationChanged(qint64 duration)
 {
-    qDebug() << duration;
+    qDebug() << "Loaded media duration: "<< duration;
     ui->sliderProgress->setMaximum(duration);
     ui->lblDuration->setText(Utils::formatDuration(duration));
 }
 
+void MediaPlayer::on_mediaChanged()
+{
+    if(!player->instance->currentMedia().isNull()){
+        qDebug() << player->instance->playlist()->objectName();
+    }
+
+}
 
 void MediaPlayer::on_actionOpenFile_triggered()
 {
@@ -61,8 +73,19 @@ void MediaPlayer::on_actionOpenFile_triggered()
     }
 }
 
-// control btns
+void MediaPlayer::on_playlistUpdated()
+{
+    qDebug() << "Media count: " << player->instance->playlist()->mediaCount();
+}
+
+// control buttons
 void MediaPlayer::on_btnPlay_clicked()
 {
     player->togglePlayPause();
 }
+
+void MediaPlayer::on_btnShuffle_toggled(bool checked)
+{
+    qDebug() << checked;
+}
+
