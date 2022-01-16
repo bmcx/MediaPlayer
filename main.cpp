1d01674a98151b9729cba4d85c6e345db31538cf
Chandima Bandara <bmcbandara@hotmail.com>
2022-01-16 13:19:14 +0530

    add: settings persistance

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    /// set a custom theme from stylesheets, do not have all the necessary styles
+    /// if uncommented below lines, it should work, but some of the UI elements
+    /// like, button toggles aren't visible. Also menu icons needs to be changes to white
+
+    // QFile styleFile(":/qss/darkeum.qss");
+    // styleFile.open(QFile::ReadOnly);
+    // QString style = QLatin1String(styleFile.readAll());
+    // a.setStyleSheet(style);
+
+    /// maybe add a theme selector to settings?
+    // a.setStyle("fusion");
+
     MediaPlayer w;
+    w.ensurePolished();
     w.show();
     return a.exec();
 }
