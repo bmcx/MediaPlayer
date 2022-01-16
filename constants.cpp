de143d84d330e7ca46646374401a49b4e614ebcf
Chandima Bandara <bmcbandara@hotmail.com>
2022-01-17 00:13:08 +0530

    add: playlist save, load features

--- constants.cpp
+++ constants.cpp
@@ -1,10 +1,13 @@
 #include "constants.h"
 
 const QString Constants::acceptedFileTypes = "Audio Files(*.mp3);;Video Files(*.mp4);;All Files(*.*)";
+const QString Constants::acceptedPlaylistFileTypes = "Playlist File(*.m3u);;All Files(*.*)";
+
 const QString Constants::creator = "BMC";
 const QString Constants::appName = "Media Player";
 
 const QString Constants::appIcon = QString(":/icons/icons/app_icon.png");
+const QString Constants::appIconWhite = QString(":/icons/icons/app_icon_w.png");
 const QString Constants::playIcon = QString(":/icons/icons/play.png");
 const QString Constants::pauseIcon = QString(":/icons/icons/pause.png");
 const QString Constants::volumeUpIcon = QString(":/icons/icons/volume_up.png");
